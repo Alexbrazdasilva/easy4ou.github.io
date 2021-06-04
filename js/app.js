8d5c598bca57d5ba19d5a7bff02c37ecfcb64021
alexbrazreal <alexbrazdasilva@gmail.com>
2021-06-04 18:04:53 -0300

    Site em desenvolvimento

---
+++ js/app.js
@@ -0,0 +1,3 @@
+// import { $ } from "./lib.js";
+const slide = document.querySelector('[data-id=slide]') 
+const carousel = new bootstrap.Carousel(slide)
---
+++ js/lib.js
@@ -0,0 +1,75 @@
+const document = window.document || self.document || this.document;
+class NoImplementation extends Error {
+  constructor(error) {
+    super(error);
+  }
+}
+
+class myLibQuery {
+  constructor(target) {
+    if (target) {
+      this.element = document.querySelector(target);
+    }
+  }
+  //   InnerValues
+  text(text) {
+    this.element.innerText = text;
+  }
+  html(html, increment = false) {
+    increment
+      ? (this.element.innerHTML += html)
+      : (this.element.innerHTML = html);
+  }
+  val(value) {
+    if (value || value == "") this.element.value = value;
+    else return this.element.value;
+  }
+  //   Attributes
+  attr(attribute, value) {
+    return value
+      ? this.element.setAttribute(attribute, value)
+      : this.element.getAttribute(attribute);
+  }
+  prop(propName) {
+    return this.element.style.getPropertyValue(propName);
+  }
+  addClass(className) {
+    this.element.classList.add(className);
+  }
+  removeClass(className) {
+    this.element.classList.remove(className);
+  }
+  hassClass(className) {
+    return this.element.classList.contains(className);
+  }
+  toggleClass(className) {
+    this.hassClass(className)
+      ? this.removeClass(className)
+      : this.addClass(className);
+  }
+  // EventListeners
+  on(event, callback) {
+    if (!this.element) {
+      throw new NoImplementation("Target is undefined or null");
+      return;
+    }
+
+    this.element.addEventListener(event, callback);
+  }
+  click(fn) {
+    this.on("click", fn);
+  }
+  focus(fn) {
+    this.on("focus", fn);
+  }
+  unFocus(fn) {
+    this.on("blur", fn);
+  }
+  change(fn) {
+    this.on("change", fn);
+  }
+  css(style) {
+    this.element.style.cssText = style;
+  }
+}
+export const $ = (element) => new myLibQuery(element);
